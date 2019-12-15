4888fb9b8ba5130abc62bdbb774c23fc844d0172
Duong Anh Son <anhson.duong@outlook.com>
2019-12-15 10:12:50 -0500

    update response

--- chap1/index.js
+++ chap1/index.js
@@ -1,7 +1,15 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
-    res.end('VNTALKING: Xin chao Node.js')
+    if (req.url === '/about')
+        res.end('The about page')
+    else if (req.url === '/contact')
+        res.end('The contact page')
+    else if (req.url === '/')
+        res.end('The home page')
+    else {
+        res.writeHead(404)
+        res.end('page not found')
+    }
 })
 server.listen(3000)
