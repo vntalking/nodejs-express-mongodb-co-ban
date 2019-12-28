214153ce34767158c5dfd42553420972dd4697f4
Duong Anh Son <anhson.duong@outlook.com>
2019-12-28 10:48:38 -0500

    Đăng ký thư mục public với express

--- chap3/index.js
+++ chap3/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = new express()
+
+//Đăng ký thư mục public.
+app.use(express.static('public'))
+
 app.listen(4000, () => {
     console.log('App listening on port 4000')
 })
