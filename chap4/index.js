3a1a43f10ba07a6a1071ba2ab2f9a2708e0ef777
Duong Anh Son <anhson.duong@outlook.com>
2019-12-30 11:28:56 -0500

    tạo ejs file

--- chap4/index.js
+++ chap4/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = new express()
 const path = require('path')
+
 const ejs = require('ejs')
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
 //Đăng ký thư mục public.
 app.use(express.static('public'))
@@ -12,6 +13,19 @@ app.listen(4000, () => {
 })
 
 
-app.get('/', (request, response) =>{
-    response.sendFile(path.resolve(__dirname, 'pages/index.html'))
+app.get('/', (request, response) => {
+    response.render('index')
+})
+
+app.get('/about', (req, res) => {
+    //res.sendFile(path.resolve(__dirname,'pages/about.html'))
+    res.render('about');
+})
+app.get('/contact', (req, res) => {
+    //res.sendFile(path.resolve(__dirname,'pages/contact.html'))
+    res.render('contact');
+})
+app.get('/post', (req, res) => {
+    //res.sendFile(path.resolve(__dirname,'pages/post.html'))
+    res.render('post')
 })
