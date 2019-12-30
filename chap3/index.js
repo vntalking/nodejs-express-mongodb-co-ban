f490dc376ecdd32bbe6a9b0f19adf498195c3726
Duong Anh Son <anhson.duong@outlook.com>
2019-12-30 10:16:46 -0500

    tạo routes cho trang tĩnh

--- chap3/index.js
+++ chap3/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = new express()
+const path = require('path')
 
 //Đăng ký thư mục public.
 app.use(express.static('public'))
 
 app.listen(4000, () => {
     console.log('App listening on port 4000')
+})
+
+
+app.get('/', (request, response) =>{
+    response.sendFile(path.resolve(__dirname, 'pages/index.html'))
 })
