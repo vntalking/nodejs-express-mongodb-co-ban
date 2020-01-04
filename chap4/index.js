c170e55c2e4ffe058d37cde80114f732336f831b
Duong Anh Son <anhson.duong@outlook.com>
2020-01-04 02:53:16 -0500

    include layouts vào các pages

--- chap4/index.js
+++ chap4/index.js
@@ -18,14 +18,11 @@ app.get('/', (request, response) => {
 })
 
 app.get('/about', (req, res) => {
-    //res.sendFile(path.resolve(__dirname,'pages/about.html'))
     res.render('about');
 })
 app.get('/contact', (req, res) => {
-    //res.sendFile(path.resolve(__dirname,'pages/contact.html'))
     res.render('contact');
 })
 app.get('/post', (req, res) => {
-    //res.sendFile(path.resolve(__dirname,'pages/post.html'))
     res.render('post')
 })
