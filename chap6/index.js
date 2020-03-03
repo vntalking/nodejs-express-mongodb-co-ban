55219013f3a0491ae2a406f3253a7fa13ef69fb6
vntalking <anhson.duong@outlook.com>
2020-03-03 23:23:37 +0700

    display post detail

--- chap6/index.js
+++ chap6/index.js
@@ -39,8 +39,12 @@ app.get('/about', (req, res) => {
 app.get('/contact', (req, res) => {
     res.render('contact');
 })
-app.get('/post', (req, res) => {
-    res.render('post')
+
+app.get('/post/:id', async (req, res) => {
+    const blogpost = await BlogPost.findById(req.params.id)
+    res.render('post', {
+        blogpost
+    })
 })
 
 app.get('/posts/new', (req, res) => {
