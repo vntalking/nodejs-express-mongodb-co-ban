af8844a65397e7e325ab179efed4d978008020a7
vntalking <anhson.duong@outlook.com>
2020-03-04 23:55:00 +0700

    add new fields

--- chap6/models/BlogPost.js
+++ chap6/models/BlogPost.js
@@ -2,8 +2,13 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 const BlogPostSchema = new Schema({
     title: String,
-    body: String
+    body: String,
+    username: String,
+    datePosted: { /* can declare property type with an object like this because we need 'default' */
+        type: Date,
+        default: new Date()
+    }
 });
 //Exportdd
-const BlogPost = mongoose.model('BlogPost',BlogPostSchema);
+const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
 module.exports = BlogPost
