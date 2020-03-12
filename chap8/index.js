385618b66ec10b64f2a3317d7475e62849d021ef
vntalking <anhson.duong@outlook.com>
2020-03-12 18:07:35 +0700

    Fix bugs

--- chap8/index.js
+++ chap8/index.js
@@ -76,7 +76,7 @@ app.get('/post/:id', (req, res) => {
 
 app.post('/posts/store', function (req, res) {
     let image = req.files.image;
-    image.mv(path.resolve(__dirname, 'public/img', image.name), function (error) {
+    image.mv(path.resolve(__dirname, 'public/upload', image.name), function (error) {
         BlogPost.create({
             ...req.body,
             image: '/upload/' + image.name
