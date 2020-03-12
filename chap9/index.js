f1c1c13efd3474fa18440353358283a9dabc022e
vntalking <anhson.duong@outlook.com>
2020-03-12 16:46:42 +0700

    add chap 9

---
+++ chap9/index.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const app = new express()
+const path = require('path')
+const ejs = require('ejs')
+app.set('view engine', 'ejs')
+
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json({ type: 'application/json' }))
+app.use(bodyParser.raw());
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true })
+
+const fileUpload = require('express-fileupload')
+app.use(fileUpload())
+
+const BlogPost = require('./models/BlogPost.js')
+
+
+//Đăng ký thư mục public.....
+app.use(express.static('public'))
+
+const customMiddleWare = (req, res, next) => {
+    console.log('Custom middle ware called')
+    next()
+}
+app.use(customMiddleWare)
+
+const validateMiddleWare = (req, res, next) => {
+    if (req.files == null || req.body.title == null || req.body.title == null) {
+        return res.redirect('/posts/new')
+    }
+    next()
+}
+app.use('/posts/new',validateMiddleWare) 
+
+//Tao server
+app.listen(4000, () => {
+    console.log('OK. App listening on port 4000')
+})
+
+
+app.get('/', (request, response) => {
+    BlogPost.find({}, function (error, posts) {
+        console.log(posts);
+        response.render('index', {
+            blogposts: posts
+        });
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+app.get('/contact', (req, res) => {
+    res.render('contact');
+})
+app.get('/post', (req, res) => {
+    res.render('post')
+})
+
+app.get('/posts/new', (req, res) => {
+    res.render('create')
+})
+
+app.post('/posts/new', validateMiddleWare)
+
