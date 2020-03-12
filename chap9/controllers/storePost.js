eacffe6ce5834684e52f59c0ccf1da36435e7107
vntalking <anhson.duong@outlook.com>
2020-03-12 21:25:52 +0700

    add 3 controllers

---
+++ chap9/controllers/storePost.js
@@ -0,0 +1,13 @@
+const BlogPost = require('../models/BlogPost.js')
+const path = require('path')
+module.exports = (req, res) => {
+    let image = req.files.image;
+    image.mv(path.resolve(__dirname, '..', '/public/upload', image.name), function (error) {
+        BlogPost.create({
+            ...req.body,
+            image: '/upload/' + image.name
+        }, function (err) {
+            res.redirect('/')
+        })
+    })
+}
