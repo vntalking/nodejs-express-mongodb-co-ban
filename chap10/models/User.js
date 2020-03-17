44fcf6ab8c1bab512ed96d69304b65f82716cf35
vntalking <anhson.duong@outlook.com>
2020-03-17 21:44:34 +0700

    mongoose validation

--- chap10/models/User.js
+++ chap10/models/User.js
@@ -3,8 +3,15 @@ const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt')
 
 const UserSchema = new Schema({
-    username: String,
-    password: String
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
 
 UserSchema.pre('save', function (next) {
