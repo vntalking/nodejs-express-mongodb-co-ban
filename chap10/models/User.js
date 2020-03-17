ad57ceb6ccfb98a79d1f794b78f07a0c31c2eaac
vntalking <anhson.duong@outlook.com>
2020-03-17 20:37:28 +0700

    add hash password

--- chap10/models/User.js
+++ chap10/models/User.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt')
 
 const UserSchema = new Schema({
     username: String,
     password: String
 });
+
+UserSchema.pre('save', function (next) {
+    const user = this
+    bcrypt.hash(user.password, 10, (error, hash) => {
+        user.password = hash
+        next()
+    })
+})
+
 // export model
 const User = mongoose.model('User', UserSchema);
 module.exports = User
